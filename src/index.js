8a444ac0eef408a45b80db70609a60e380d7a124
Sarkis <sarkis.tlt@gmail.com>
2017-03-20 13:02:14 -0700

    minor improvement and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,97 +1,118 @@
 import Schedule from 'schedule-js';
 
 export default function webhook(config) {
-    let http = require('http'),
-        createHandler = require('github-webhook-handler'),
-        handler = config.github ? createHandler({path: config.github.path, secret: config.github.secret}) : false,
-        spawn = require('child_process').spawn,
-        slack = config.slack ? require('slack-notify')(config.slack.url) : false;
+  const http = require('http');
+  const createHandler = require('github-webhook-handler');
+  const handler = config.github ? createHandler({ path: config.github.path, secret: config.github.secret }) : false;
+  const spawn = require('child_process').spawn;
+  const slack = config.slack ? require('slack-notify')(config.slack.url) : false;
 
-    http.createServer((req, res) => {
-        handler(req, res, err => {
-            res.statusCode = 404;
-            res.end('no such location');
-        });
-    }).listen(config.port);
-    handler.on('error', err => console.error(`Error: ${err.message}`));
+  http.createServer((req, res) => {
+    handler(req, res, (err) => {
+      res.statusCode = 404;
+      res.end('no such location');
+    });
+  }).listen(config.port);
+  handler.on('error', err => console.error(`Error: ${err.message}`));
 
-    let gitPull = (event) => {
-        let gitReset = spawn('git', ['reset', '--hard'], {cwd: config.github.projectRoot});
-        gitReset.on('exit', code => {
-            let gitProcess = spawn('git', ['pull'], {cwd: config.github.projectRoot});
-            console.log('on exit');
-            gitProcess.stdout.pipe(process.stdout);
-            gitProcess.stderr.pipe(process.stderr);
-            if (event) {
-                gitProcess.on('exit', code => {
-                    slack.send({
-                        channel: `#${config.slack.channel}`,
-                        text: `${config.slack.projectName}, pointing at ${event.payload.ref} has been updated`,
-                        unfurl_links: 1,
-                        username: 'webhook',
-                        fields: {
-                            'Commit': event.payload.head_commit.id,
-                            'Message': event.payload.head_commit.message,
-                            'Author': event.payload.head_commit.author.name
-                        }
-                    });
-                    if (config.hasOwnProperty('exec')) {
-                        let exec = require('child_process').exec;
-                        exec(config.exec,
-                            function (error, stdout, stderr) {
-                                console.log('stdout: ' + stdout);
-                                console.log('stderr: ' + stderr);
-                                if (error !== null) {
-                                    console.log('exec error: ' + error);
-                                }
-                            });
-                    }
+  const gitPull = (event) => {
+    const gitReset = spawn('git', ['reset', '--hard'], { cwd: config.github.projectRoot });
+    gitReset.on('exit', () => {
+      const gitProcess = spawn('git', ['pull'], { cwd: config.github.projectRoot });
+      console.log('git has reset');
+      gitProcess.stdout.pipe(process.stdout);
+      gitProcess.stderr.pipe(process.stderr);
+      if (event) {
+        gitProcess.on('exit', () => {
+          console.log('git pulled');
+          slack.send({
+            channel: `#${config.slack.channel}`,
+            text: `${config.slack.projectName}, pointing at ${event.payload.ref} has been updated`,
+            unfurl_links: 1,
+            username: 'webhook',
+            fields: {
+              Commit: event.payload.head_commit.id,
+              Message: event.payload.head_commit.message,
+              Author: event.payload.head_commit.author.name,
+            },
+          });
+          if (config.hasOwnProperty('exec')) {
+            const exec = require('child_process').exec;
+            if (typeof (config.exec) === 'string') {
+              exec(config.exec,
+                (error, stdout, stderr) => {
+                  console.log(`stdout: ${stdout}`);
+                  console.log(`stderr: ${stderr}`);
+                  if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                  }
                 });
-            } else {
-                gitProcess.on('exit', code => {
-                    slack.send({
-                        channel: `#${config.slack.channel}`,
-                        text: `${config.slack.projectName}, scheduled "git pull"`,
-                        unfurl_links: 1,
-                        username: 'webhook'
-                    });
-                    if (config.hasOwnProperty('exec')) {
-                        let exec = require('child_process').exec;
-                        if (typeof (config.exec) === 'string') {
-                            exec(config.exec,
-                                function (error, stdout, stderr) {
-                                    console.log('stdout: ' + stdout);
-                                    console.log('stderr: ' + stderr);
-                                    if (error !== null) {
-                                        console.log('exec error: ' + error);
-                                    }
-                                });
-                        } else if (config.exec[0]) {
-                            let execCommand = '';
-                            config.exec.forEach((command, idx) => {
-                                if (idx === config.exec.length - 1) {
-                                    execCommand += command;
-                                } else {
-                                    execCommand += command + ' && ';
-                                }
-                            });
-                            exec(execCommand,
-                                function (error, stdout, stderr) {
-                                    console.log('stdout: ' + stdout);
-                                    console.log('stderr: ' + stderr);
-                                    if (error !== null) {
-                                        console.log('exec error: ' + error);
-                                    }
-                                });
-                        }
-                    }
+            } else if (config.exec[0]) {
+              let execCommand = '';
+              config.exec.forEach((command, idx) => {
+                if (idx === config.exec.length - 1) {
+                  execCommand += command;
+                } else {
+                  execCommand += `${command} && `;
+                }
+              });
+              exec(execCommand,
+                (error, stdout, stderr) => {
+                  console.log(`stdout: ${stdout}`);
+                  console.log(`stderr: ${stderr}`);
+                  if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                  }
                 });
             }
+          }
         });
-    };
+      } else {
+        gitProcess.on('exit', () => {
+          console.log('git pulled');
+          slack.send({
+            channel: `#${config.slack.channel}`,
+            text: `${config.slack.projectName}, scheduled "git pull"`,
+            unfurl_links: 1,
+            username: 'webhook',
+          });
+          if (config.hasOwnProperty('exec')) {
+            const exec = require('child_process').exec;
+            if (typeof (config.exec) === 'string') {
+              exec(config.exec,
+                (error, stdout, stderr) => {
+                  console.log(`stdout: ${stdout}`);
+                  console.log(`stderr: ${stderr}`);
+                  if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                  }
+                });
+            } else if (config.exec[0]) {
+              let execCommand = '';
+              config.exec.forEach((command, idx) => {
+                if (idx === config.exec.length - 1) {
+                  execCommand += command;
+                } else {
+                  execCommand += `${command} && `;
+                }
+              });
+              exec(execCommand,
+                (error, stdout, stderr) => {
+                  console.log(`stdout: ${stdout}`);
+                  console.log(`stderr: ${stderr}`);
+                  if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                  }
+                });
+            }
+          }
+        });
+      }
+    });
+  };
 
-    handler.on('push', event => gitPull(event));
-    if (config.hasOwnProperty('schedule')) {
-        Schedule.scheduleAt(config.schedule[0], config.schedule[1], gitPull);
-    }
+  handler.on('push', gitPull);
+  if (config.hasOwnProperty('schedule')) {
+    Schedule.scheduleAt(config.schedule[0], config.schedule[1], gitPull);
+  }
 }
